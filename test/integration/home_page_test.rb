43357963ebd2c711b2a68f6425d4b865d3b642e0
Michael Lewandowski <milewgit@gmail.com>
2014-08-19 22:17:59 -0400

    Add the main menu (currently, only a home link)

--- test/integration/home_page_test.rb
+++ test/integration/home_page_test.rb
@@ -1,14 +1,27 @@
 require 'test_helper'
 
+# See https://github.com/metaskills/minitest-spec-rails#test-styles
 class IntegrationTest < ActionDispatch::IntegrationTest
 
   describe 'the home page' do
-    describe 'when it is loaded' do
-      before do
-        visit '/'
-      end
-      it 'displays "home page"' do
-        page.must_have_content 'home page'
+    before do
+      visit '/'
+    end
+    it 'displays "home page"' do
+      page.must_have_content 'home page'
+    end
+    it 'display the main menu' do
+      page.must_have_selector 'nav#main-menu'
+    end
+  end
+  
+  describe 'the main menu' do
+    before do
+      visit '/'
+    end
+    it 'has a link to the home page' do
+      within 'nav#main-menu' do
+        page.must_have_link 'Biodb'
       end
     end
   end
