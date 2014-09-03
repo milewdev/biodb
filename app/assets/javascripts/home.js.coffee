fea8cc61b526ef54f30f8820f7e54c8f4cdc3f99
Michael Lewandowski <milewgit@gmail.com>
2014-09-03 15:30:10 -0400

    Implement Enter and Backspace editing in highlights

--- app/assets/javascripts/home.js.coffee
+++ app/assets/javascripts/home.js.coffee
@@ -2,11 +2,52 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
+textarea_keydown = (event) ->
+  if event.which == 13      # enter (TODO: use a constant)
+    
+    # insert new li
+    existing_textarea = $(event.target)
+    existing_parent_ul = existing_textarea.parent().get(0)
+    new_textarea = $("<textarea data-id='-1'></textarea>")
+    new_li = $("<li></li>")
+    new_li.append(new_textarea)
+    new_li.insertAfter(existing_parent_ul)
+    
+    # move everything after the current cursor position to the new textarea
+    # TODO: need to handle case when the user has selected some text
+    new_textarea_text = existing_textarea.range(existing_textarea.caret()).range().text
+    new_textarea.val(new_textarea_text)
+    existing_textarea.range(existing_textarea.caret()).range('')
+
+    # TODO: trim spaces from end of existing_textarea after split?  But then do we
+    # add a space when we join via the delete key?
+
+    # position the cursor
+    new_textarea.focus()
+
+    # eat the return
+    return false 
+    
+  if event.which == 8       # backspace (TODO: use a constant)
+    current_textarea = $(event.target)
+    if current_textarea.caret() == 0    # at start of line
+      prev_li = current_textarea.parent().prev()[0]
+      if not $.isEmptyObject(prev_li)   # not on the first highlight
+        prev_li = $(prev_li)
+        prev_textarea = prev_li.children().first()
+        new_caret_position = prev_textarea.val().length
+        prev_textarea.val( prev_textarea.val() + current_textarea.val() )
+        current_textarea.parent().remove()
+        prev_textarea.focus()
+        prev_textarea.caret(new_caret_position)
+        return false
+
 display_data = (highlights) ->
   list = $('.highlights')
   for highlight in highlights
     list.append "<li><textarea data-id='#{highlight.id}'>#{highlight.content}</textarea></li>"
   
 $(document).ready -> 
+  $('ul.highlights').on('keydown', 'textarea', textarea_keydown)
   user_id = $('.highlights')?.data('user-id')
   $.ajax(url: "/users/#{user_id}/highlights.json").done(display_data) if user_id?
--- test/controllers/views/home_page_test.rb
+++ test/controllers/views/home_page_test.rb
@@ -87,20 +87,135 @@ class HomePageTest < ActionDispatch::IntegrationTest
   end
   
   describe 'the highlight list' do
+    
     before do
       enable_js
       sign_in
-    end
-    it 'displays all the highlights of the logged in user' do
       visit home_path
+    end
+    
+    it 'displays all the highlights of the logged in user' do
       users(:name1).highlights.each do |highlight|
         find("textarea[data-id='#{highlight.id}']").value.must_equal highlight.content
       end
     end
+    
+    let(:first_id) { highlights(:highlight1_1).id }
+    let(:second_id) { highlights(:highlight1_2).id }
+    it 'displays the highlights in sort order' do
+      page.source.must_match /data-id="#{first_id}".*data-id="#{second_id}"/
+    end
+    
+    describe 'when text is entered at the end of a highlight field' do
+      let(:highlight) { highlights(:highlight1_1) }
+      let(:highlight_field) { find("textarea[data-id='#{highlight.id}']") }
+      before do
+        highlight_field.native.send_keys(' new content')
+      end
+      it 'is appended to the highlight value' do
+        highlight_field.value.must_equal highlight.content + ' new content'
+      end
+    end
+    
+    describe 'when <return> is pressed at the end of a highlight field' do
+      let(:highlight1) { highlights(:highlight1_1) }
+      let(:highlight2) { highlights(:highlight1_2) }
+      let(:highlight1_field) { find("textarea[data-id='#{highlight1.id}']") }
+      before do
+        highlight1_field.native.send_key(:Enter)
+      end
+      it 'inserts a new highlight field after the one in which enter was pressed' do
+        page.all('textarea')[0]['data-id'].must_equal "#{highlight1.id}"
+        page.all('textarea')[1]['data-id'].must_equal '-1'
+        page.all('textarea')[2]['data-id'].must_equal "#{highlight2.id}"
+      end
+      it 'sets the new highlight field to blank' do
+        page.all('textarea')[1].value.must_equal ''
+      end
+      it 'sets the new highlight field id to -1' do
+        page.all('textarea')[1]['data-id'].must_equal '-1'
+      end
+      it 'puts the cursor at the beginning of the new highlight' do
+        # TODO: this only tests that the cursor is in the new highlight field,
+        # not that it is at the begining of the field.
+        page.evaluate_script("document.activeElement.getAttribute('data-id')").must_equal '-1'
+      end
+    end
+    
+    describe 'when <return> is pressed in the middle of a highlight field' do
+      let(:highlight1) { highlights(:highlight1_1) }
+      let(:highlight2) { highlights(:highlight1_2) }
+      let(:highlight1_field) { find("textarea[data-id='#{highlight1.id}']") }
+      before do
+        highlight1_field.native.send_keys(:Left, :Left, :Left, :Enter)
+      end
+      it 'removes everything after the cursor location from the highlight field' do
+        highlight1_field.value.must_equal highlights(:highlight1_1).content[0..-4]
+      end
+      it 'adds everything after the cursor location to the newly inserted highlight field' do
+        page.all('textarea')[1].value.must_equal highlights(:highlight1_1).content[-3..-1]
+      end
+    end
+    
+    describe 'when <delete> is pressed at the beginning of the first highlight field' do
+      let(:highlight1) { highlights(:highlight1_1) }
+      let(:highlight1_field) { find("textarea[data-id='#{highlight1.id}']") }
+      before do
+        highlight1_field.native.send_keys(:Home, :Backspace)
+      end
+      it 'does nothing' do
+        highlight1_field.value.must_equal highlight1.content
+      end
+    end
+    
+    describe 'when <delete> is pressed at the beginning of the second highlight field' do
+      let(:highlight1) { highlights(:highlight1_1) }
+      let(:highlight2) { highlights(:highlight1_2) }
+      let(:highlight1_field) { find("textarea[data-id='#{highlight1.id}']") }
+      let(:highlight2_field) { find("textarea[data-id='#{highlight2.id}']") }
+      before do
+        highlight2_field.native.send_keys(:Home, :Backspace)
+      end
+      it 'append the content of the second highlight field to the first one' do
+        highlight1_field.value.must_equal highlight1.content + highlight2.content
+      end
+      it 'deletes the second highlight field' do
+        page.wont_have_css "textarea[data-id='#{highlight2.id}']"
+      end
+      it 'leaves the focus on the first highlight field' do
+        page.evaluate_script("document.activeElement.getAttribute('data-id')").must_equal "#{highlight1.id}"
+      end
+      it 'leaves the caret at the content join point' do
+        page.evaluate_script("$(document.activeElement).caret()").must_equal highlight1.content.length
+      end
+    end
+    
+    describe 'when <delete> is pressed anywhere other than the beginning of the second highlight field' do
+      let(:highlight1) { highlights(:highlight1_1) }
+      let(:highlight2) { highlights(:highlight1_2) }
+      let(:highlight1_field) { find("textarea[data-id='#{highlight1.id}']") }
+      let(:highlight2_field) { find("textarea[data-id='#{highlight2.id}']") }
+      before do
+        highlight2_field.native.send_keys(:Home, :Right, :Right, :Backspace)
+      end
+      it 'deletes the character to the left of the caret in the second highlight field' do
+        expected_content = highlight2.content
+        expected_content[1] = ''
+        highlight2_field.value.must_equal expected_content
+      end
+      it 'does not change the contents of the first highlight field' do
+        highlight1_field.value.must_equal highlight1.content
+      end
+      it 'does not delete the second highlight field' do
+        page.must_have_css "textarea[data-id='#{highlight2.id}']"
+      end
+    end
+    
     after do
       sign_out
       disable_js
     end
+    
   end
 
 end
