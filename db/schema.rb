cc67a5826533bbabbd26a400f6afd58c983c94b2
Michael Lewandowski <milewgit@gmail.com>
2014-09-04 10:14:56 -0400

    Create migration to add title to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140820121442) do
+ActiveRecord::Schema.define(version: 20140904141407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140820121442) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title"
   end
 
 end
