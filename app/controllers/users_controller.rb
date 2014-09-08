8cc980ff2a3898fb6a1015e6110e87f7750f6374
Michael Lewandowski <milewgit@gmail.com>
2014-09-08 14:45:02 -0400

    Add title to users controller permitted params.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+        format.json { render :show, status: :ok, location: @user }    # TODO: do not return the user data
       else
         format.html { render :edit }
         format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
+      params.require(:user).permit(:email, :password, :password_confirmation, :title)
     end
 end
